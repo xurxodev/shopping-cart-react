22342223c957ac994e21e9ae6c3f397e6fe46a17
Jorge Sánchez <xurxosf@gmail.com>
2019-11-09 09:03:12 +0100

    Create ProductItem component

--- lifting-state-up/src/components/ProductList.tsx
+++ lifting-state-up/src/components/ProductList.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  Container,
-  Grid,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActions,
-  Button,
-  Box
-} from "@material-ui/core";
+import { Container, Grid, Typography, Box } from "@material-ui/core";
 import GetProductsUseCase from "../core/domain/GetProductsUseCase";
 import Product from "../core/domain/Product";
-
-//import tileData from './tileData';
+import ProductItem from "./ProductItem";
 
 const useStyles = makeStyles(theme => ({
   titleContainer: {
@@ -23,30 +12,6 @@ const useStyles = makeStyles(theme => ({
   cardGrid: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4)
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column"
-  },
-  cardMedia: {
-    backgroundSize: "auto 100%",
-    paddingTop: "100%", // 16:9,
-    margin: theme.spacing(1)
-  },
-  cardContent: {
-    flexGrow: 1
-  },
-  cardActions: {
-    justifyContent: "center"
-  },
-  productTitle: {
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    height: 50
-  },
-  productPrice: {
-    textAlign: "center"
   }
 }));
 
@@ -85,32 +50,7 @@ const ProductList: React.FC<AppProps> = ({ getProductsUseCase }) => {
       </Box>
       <Grid container spacing={2}>
         {products.map(product => (
-          <Grid item xs={6} sm={4} md={3} lg={2}>
-            <Card className={classes.card}>
-              <CardMedia
-                className={classes.cardMedia}
-                image={product.image}
-                title="Image title"
-              />
-              <CardContent className={classes.cardContent}>
-                <Typography
-                  className={classes.productTitle}
-                  gutterBottom
-                  variant="subtitle1"
-                >
-                  {product.title}
-                </Typography>
-                <Typography variant="h6" className={classes.productPrice}>
-                  {product.price}
-                </Typography>
-              </CardContent>
-              <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary">
-                  Add to Cart
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+          <ProductItem product={product} />
         ))}
       </Grid>
     </Container>
